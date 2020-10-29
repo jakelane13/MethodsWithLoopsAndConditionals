99d6a71db987f6576c3b80138765e326a79b2c60
Jake Lane <lanejake13@gmail.com>
2020-10-29 14:37:57 -0400

    Add project files.

---
+++ MethodsWithLoopsAndConditionals/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MethodsWithLoopsAndConditionals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //PrintRange();
+            //Threes();
+        }
+
+        public static void Threes()
+        {
+            for (int i = 3; i <= 999; i += 3)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        public static void PrintRange()
+        {
+            for (int i = 1000; i >= -1000; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
